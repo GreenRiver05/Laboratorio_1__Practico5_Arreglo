128fc46e383d279f30da41ef71b50449633f675c
alex-astudillo <astudillooalexx@gmail.com>
2023-04-20 11:26:04 -0300

    Agrego sumarLista

--- src/practico5_arreglo/Arreglo.java
+++ src/practico5_arreglo/Arreglo.java
@@ -1,14 +1,17 @@
-
 package practico5_arreglo;
 
-
 public class Arreglo {
-    
-    public void static sumarLista(int[] arreglo){
-        
-            for (int i = 0; i<arreglo.length() ; i++) {
-                
-            }
-        
+
+    public static void sumarLista(int[] arreglo) {
+        int suma = 0;
+        int promedio = 0;
+        for (int i = 1; i < arreglo.length; i++)
+        {
+            suma += arreglo[i];
+            promedio = suma / arreglo.length;
+        }
+        System.out.println("La suma de los numeros es > " + suma + " y el promedio es > " + promedio);
     }
+
+    
 }
