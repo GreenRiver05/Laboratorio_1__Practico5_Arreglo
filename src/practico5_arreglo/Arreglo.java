06c334d887016902ff0eda42fffb9d0096fabce5
GreenRiver05 <gabrielbd94@gmail.com>
2023-04-20 12:06:59 -0300

    Metodos nuevos
    Se agrega el metodo cuentaVocales() y el buscarMayor()

--- src/practico5_arreglo/Arreglo.java
+++ src/practico5_arreglo/Arreglo.java
@@ -2,16 +2,42 @@ package practico5_arreglo;
 
 public class Arreglo {
 
-    public static void sumarLista(int[] arreglo) {
+    static void sumarLista(int[] arreglo) {
         int suma = 0;
         int promedio = 0;
-        for (int i = 1; i < arreglo.length; i++)
-        {
+        for (int i = 1; i < arreglo.length; i++) {
+
             suma += arreglo[i];
             promedio = suma / arreglo.length;
         }
         System.out.println("La suma de los numeros es > " + suma + " y el promedio es > " + promedio);
     }
 
+    static void buscarMayor(int[][] bidimensional){
+        int mayor=0;
+        
+//         for (int i = 0; i <bidimensional.length; i++) {
+//         for (int j = 0; j <bidimensional[i].length; j++) {
+        for (int[] bidimensional1 : bidimensional) {
+            for (int j = 0; j < bidimensional1.length; j++) {
+                if (bidimensional1[j] > mayor) {
+                    mayor = bidimensional1[j];
+                }
+            }
+        }
+         System.out.println("El numero mayor es >" + mayor);
+    }
     
+    
+    static void cuentaVocales(String cadena){
+        int vocales=0;
+        for (int i = 0; i<cadena.length(); i++) {
+    
+             if(cadena.equalsIgnoreCase("a")){
+                 vocales++;
+             }
+        } 
+        System.out.println(vocales);
+    }
+            
 }
--- src/practico5_arreglo/Practico5_Arreglo.java
+++ src/practico5_arreglo/Practico5_Arreglo.java
@@ -8,7 +8,13 @@ public class Practico5_Arreglo {
         
         int[] arreglo = {2,4,6,8,10,12,14};
         Arreglo.sumarLista(arreglo);
+        
+        int[][] bidimensional = {{5,7,10,15,35,4},{3,9,20}};
+        Arreglo.buscarMayor(bidimensional);
             
+        String cadena = "contamos todas las vocales";
+        Arreglo.cuentaVocales(cadena);
+        
         
     }
     
