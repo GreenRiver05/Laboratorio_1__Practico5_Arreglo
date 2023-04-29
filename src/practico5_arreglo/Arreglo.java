4905cbbbcbf685da24dece54bd147c496cb8f74f
GreenRiver05 <gabrielbd94@gmail.com>
2023-04-29 20:41:37 -0300

    metodo A y B  ; color y orden
    se agrego para que se muestre los arreglos del A y del B, y de los demás solo se agrego color y se ordeno

--- src/practico5_arreglo/Arreglo.java
+++ src/practico5_arreglo/Arreglo.java
@@ -2,16 +2,23 @@ package practico5_arreglo;
 
 public class Arreglo {
 
+    public static final String nada = "\u001B[0m";               // se utilizo ANSI escape code, color caractares
+    public static final String rojo = "\u001B[31m";             // se utilizo ANSI escape code, color caractares
+    public static final String verde = "\u001B[32m";           // se utilizo ANSI escape code, color caractares
+    public static final String amarillo = "\u001B[33m";         // se utilizo ANSI escape code, color caractares
+    public static final String celeste = "\u001B[36m";          // se utilizo ANSI escape code, color caractares
+    public static final String negroF = "\u001B[40m";           // se utilizo ANSI escape code, color caractares
+
     static void sumarLista(int[] arreglo) {
         int suma = 0;
         int promedio = 0;
-        for (int i = 1; i < arreglo.length; i++)
-        {
+        for (int i = 1; i < arreglo.length; i++) {
 
             suma += arreglo[i];
             promedio = suma / arreglo.length;
+            System.out.print(arreglo[i] + amarillo + " | " + nada);
         }
-        System.out.println("La suma de los numeros es > " + suma + " y el promedio es > " + promedio);
+        System.out.println(amarillo + "\n\n↑ " + nada + " La suma de los numeros es  " + amarillo + suma + nada + " y el promedio es " + amarillo + promedio);
     }
 
     static void buscarMayor(int[][] bidimensional) {
@@ -19,39 +26,35 @@ public class Arreglo {
 
 //         for (int i = 0; i <bidimensional.length; i++) {
 //         for (int j = 0; j <bidimensional[i].length; j++) {
-        for (int[] bidimensional1 : bidimensional)
-        {
-            for (int j = 0; j < bidimensional1.length; j++)
-            {
-                if (bidimensional1[j] > mayor)
-                {
-                    mayor = bidimensional1[j];
+        for (int i = 0; i < bidimensional.length; i++) {
+            for (int j = 0; j < bidimensional[i].length; j++) {
+                if (bidimensional[i][j] > mayor) {
+                    mayor = bidimensional[i][j];
                 }
+                System.out.print(bidimensional[i][j] + celeste  + " | " + nada);
             }
+            System.out.println("\n");
         }
-        System.out.println("El numero mayor es >" + mayor);
+        System.out.println(celeste + "↑ " + nada + "El numero mayor es "+ celeste + mayor);
     }
 
     static void cuentaVocales(String cadena) {
         int vocales = 0;
-        for (int i = 0; i < cadena.length(); i++)
-        {
+        for (int i = 0; i < cadena.length(); i++) {
 
-            if (cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' || cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || cadena.charAt(i) == 'u')
-            {
+            if (cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' || cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || cadena.charAt(i) == 'u') {
                 vocales++;
             }
         }
-        System.out.println(vocales);
+        System.out.println("La Cantidad de vocales que tiene la cadena " + verde + "''" + nada 
+                + "contamos todas las vocales" + verde + "''" + nada + " es " + verde + vocales);
     }
 
     static int cuentaCaracter(String cadena, char caracter) {
         int contador = 0;
-        for (int i = 0; i < cadena.length(); i++)
-        {
+        for (int i = 0; i < cadena.length(); i++) {
             char c = cadena.charAt(i);
-            if (c == caracter || Character.toUpperCase(c) == Character.toUpperCase(caracter) || Character.toLowerCase(c) == Character.toLowerCase(caracter))
-            {
+            if (c == caracter || Character.toUpperCase(c) == Character.toUpperCase(caracter) || Character.toLowerCase(c) == Character.toLowerCase(caracter)) {
                 contador++;
             }
         }
---
+++ src/practico5_arreglo/PruebaArreglo.java
@@ -0,0 +1,54 @@
+package practico5_arreglo;
+
+//              GRUPO 9
+//INTEGRANTES : Pablo Nicolas Peñaloza
+//              Alex Gustavo Astudillo Duran
+//              Gabriel Ezequiel Becerra Diaz
+public class PruebaArreglo {
+    public static final String nada = "\u001B[0m";               // se utilizo ANSI escape code, color caractares
+    public static final String rojo = "\u001B[31m";             // se utilizo ANSI escape code, color caractares
+    public static final String verde = "\u001B[32m";           // se utilizo ANSI escape code, color caractares
+    public static final String amarillo = "\u001B[33m";         // se utilizo ANSI escape code, color caractares
+    public static final String celeste = "\u001B[36m";          // se utilizo ANSI escape code, color caractares
+    public static final String negroF = "\u001B[40m";           // se utilizo ANSI escape code, color caractares    
+    public static void main(String[] args) {
+
+        System.out.println(amarillo + "A) Método sumarLista:" + nada + " recibe por parámetro un arreglo unidimensional de\n"
+                + "enteros y muestre por pantalla la suma y promedio de los mismos.\n");
+
+        int[] arreglo = {2, 4, 6, 8, 10, 12, 14};
+        
+        Arreglo.sumarLista(arreglo);
+        
+        
+        System.out.println("\n\n____________________________________________________________________________________________________\n\n");
+
+        System.out.println(celeste + "B) Método buscarMayor:" + nada + " recibe por parámetro un arreglo bidimensional e\n"
+                + "irregular de enteros y retorne el entero más grande que se encuentra en el arreglo.\n");
+
+        int[][] bidimensional = {{5, 7, 10, 15, 35, 4}, {3, 9, 20}};
+        Arreglo.buscarMayor(bidimensional);
+        
+        
+         System.out.println("\n\n____________________________________________________________________________________________________\n\n");
+
+        System.out.println(verde + "C) Método cuentaVocales: " + nada + "recibe por parámetro un String y retorne la cantidad\n"
+                + "de vocales que tiene la cadena.\n");
+
+        String cadena = "contamos todas las vocales";
+        Arreglo.cuentaVocales(cadena);
+        
+        
+         System.out.println("\n\n____________________________________________________________________________________________________\n\n");
+
+        System.out.println(rojo + "D) Método cuentaCaracter:" + nada + "recibe por parámetro un String y un caracter, luego\n"
+                + "retorne la cantidad de veces que se repite en la cadena el carácter recibido.\n");
+        String cadena2 = "Usando GitHub Desktop";
+        
+        char caracter = 'u';
+        int contadorCaracter = Arreglo.cuentaCaracter(cadena2, caracter);
+        System.out.println("La cantidad de veces que se repite el caracter " + rojo + "'" + caracter + "'"
+        + nada + " en la cadena" + rojo + " ''" + nada + cadena2 + rojo + "''" + nada + " es: " + rojo + contadorCaracter + "\n\n");
+    }
+
+}
